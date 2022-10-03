2fd73fa17eab2273a00bd074551e024665f0c84a
seuxkj <1538890100@qq.com>
2022-10-03 11:13:04 +0800

    initial

---
+++ pages/INFO/nCoVinfo.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<u-subsection :list="list" :current="curNow" @change="sectionChange"></u-subsection>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: ['国内疫情', '国外疫情', '核酸检测点信息','防疫政策'],
+				curNow: 1
+			}
+		},
+		methods: {
+			sectionChange(index) {
+				this.curNow = index;
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
